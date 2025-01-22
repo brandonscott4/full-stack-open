aedb9d18a07bc73b639074a1abc21959504623b2
brandonscott4 <bscott15@sky.com>
2025-01-22 18:22:01 +0000

    added exercise 1.9

--- Part-1/unicafe/src/Statistics.jsx
+++ Part-1/unicafe/src/Statistics.jsx
@@ -3,6 +3,15 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = (good + bad * -1) / total;
   const positive = (good / (good + neutral + bad)) * 100;
 
+  if (total === 0) {
+    return (
+      <div>
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
   return (
     <>
       <h2>statistics</h2>
