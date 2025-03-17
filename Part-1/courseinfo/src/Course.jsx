fdd62dcd7320b52e21da68e4a249128b4cb3adcc
brandonscott4 <bscott15@sky.com>
2025-03-17 16:47:48 +0000

    added exercise 2.1

---
+++ Part-1/courseinfo/src/Course.jsx
@@ -0,0 +1,26 @@
+const Header = ({ courseName }) => <h1>{courseName}</h1>;
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+);
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
--- Part-1/courseinfo/src/Header.jsx
+++
@@ -1,5 +0,0 @@
-const Header = (props) => {
-  return <h1>{props.courseName}</h1>;
-};
-
-export default Header;
